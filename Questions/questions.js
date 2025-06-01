2fb2b8c77c9f0217982cad603b0079d92c73c91c
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-01 20:56:41 +0530

    Update new questions

--- Questions/questions.js
+++ Questions/questions.js
@@ -155,3 +155,61 @@ var searchInsert = function(nums, target) {
     }
     return start;
 }
+
+
+{/* 
+    540. Single Element in a Sorted Array
+
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+ 
+Example 1:
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+    
+*/}
+
+var singleNonDuplicate = function (nums) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  if (nums.length === 1) return nums[0];
+  if (nums[start] !== nums[start + 1]) return nums[start];
+  if (nums[end] !== nums[end - 1]) return nums[end];
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1])
+      return nums[mid];
+
+    if (mid % 2 === 0) {
+      if (nums[mid] === nums[mid - 1]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (nums[mid] === nums[mid - 1]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+};
