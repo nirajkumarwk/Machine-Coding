81e218b3695cfbe79c6cfbc9c65c33377efff102
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-02 22:50:19 +0530

    Minimum Size Subarray Sum

--- Questions/questions.js
+++ Questions/questions.js
@@ -274,3 +274,58 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+
+
+{ /* 
+    
+209. Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+    */}
+
+
+var minSubArrayLen = function(target, nums) { 
+    let start = 0;
+    let sum = 0;
+    let minLength = Infinity;
+
+    for(let end=0; end < nums.length; end++) {
+        sum += nums[end];
+
+        while(sum >= target) {
+            minLength = Math.min(minLength, end - start + 1);
+            sum -= nums[start];
+            start++;
+        }
+    }
+  return minLength === Infinity ? 0 : minLength; 
+
+}
