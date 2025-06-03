db1e9f8f4cb49f69fa49d0bb1b61e27421f33981
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-03 14:05:21 +0530

    Longest substring

--- Questions/questions.js
+++ Questions/questions.js
@@ -20,7 +20,7 @@ function BinarySearch(nums, target) {
     return -1;
 }
 
-console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11))
+// console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11))
 
 
 { /*  
@@ -329,3 +329,66 @@ var minSubArrayLen = function(target, nums) {
   return minLength === Infinity ? 0 : minLength; 
 
 }
+
+
+
+
+
+{/* 
+
+3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
+
+*/}
+
+var lengthOfLongestSubstring = function(s) {
+    let seen = new Set();
+    let start = 0;
+    let maxLength = 0;
+    let substring;
+
+    for(let end=0; end<s.length; end++) {
+        while(seen.has(s[end])){
+            seen.delete(s[start]);
+            start++
+        }
+
+        seen.add(s[end]);
+        let currentlength = end - start + 1;
+        if(currentlength > maxLength) {
+            maxLength = currentlength;
+            substring = s.slice(start, end+1)
+        }
+        
+        
+    }
+    return `maxlength ${maxLength} and substring ${substring}`
+};
+
