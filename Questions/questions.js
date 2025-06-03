364a11d326005284396caa469bb643fb9d75d7f8
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-03 15:06:30 +0530

    Longest Repeating Character Replacement

--- Questions/questions.js
+++ Questions/questions.js
@@ -392,3 +392,58 @@ var lengthOfLongestSubstring = function(s) {
     return `maxlength ${maxLength} and substring ${substring}`
 };
 
+
+
+{ /* 
+    
+424. Longest Repeating Character Replacement
+
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length   
+    
+    */}
+
+
+
+var characterReplacement = function(s, k) {
+    let start = 0;
+    let count = {};
+    let maxLength = 0;
+    let maxCount = 0;
+
+    for(let end=0; end<s.length; end++) {
+        count[s[end]] = (count[s[end]] || 0) + 1;
+        maxCount = Math.max(maxCount, count[s[end]])
+
+        if((end - start + 1) - maxCount > k) {
+            count[s[end]]--;
+            start++
+        };
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+    return maxLength;
+}
+
