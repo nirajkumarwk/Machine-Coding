184ea794863f9205774a88f6d3cb97f1b7db659f
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-04 20:57:42 +0530

    Palindrome Number

--- Questions/questions.js
+++ Questions/questions.js
@@ -535,3 +535,56 @@ var twoSum = function (nums, target) {
     map.set(nums[i], i);
   }
 };
+
+
+
+
+{/* 
+    
+9. Palindrome Number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?  
+    
+*/}
+
+
+var isPalindrome = function (x) {
+  let str = String(x);
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start <= end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
+};
