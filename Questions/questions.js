3e3d6f89b7e11b61c4f80bab51a6a7160cf2dc18
Niraj Kumar <nirajkumarwk@gmail.com>
2025-06-11 22:46:30 +0530

    Optimised Approach

--- Questions/questions.js
+++ Questions/questions.js
@@ -1505,3 +1505,38 @@ var threeSum = function (nums) {
     }
     return arr;
 }
+
+// optimised approach
+var threeSum = function (nums) {
+    let n = nums.length;
+    let arr = [];
+    nums.sort((a, b) => a-b);
+
+    for(let i=0; i<n-2; i++) {
+        
+        if(i !== 0 && nums[i] === nums[i-1]) continue;
+
+        let j = i + 1;
+        let k = n - 1;
+
+        while(j < k) {
+
+            let sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0) {
+                j++;
+            } else if(sum > 0) {
+                k--;
+            } else {
+                let temp = [nums[i], nums[j], nums[k]];
+                arr.push(temp);
+                j++; 
+                k--;
+
+                while(j < k  && nums[j] === nums[j-1]) j++;
+                while(j < k  && nums[k] === nums[k+1]) k--;
+            }
+
+        }
+    }
+    return arr;
+}
